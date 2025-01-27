b84f0ecfdd7d17ce870b70ec1967dc12d5be8256
Laura Valentine Tscharner <laura.tscharner@tecnico.ulisboa.pt>
2025-01-27 17:33:10 +0100

    cleanup

--- scripts/build.py
+++ scripts/build.py
@@ -144,8 +144,6 @@ def parse_yaml(recipe_file: str) -> AppRecipe:
             for apk in vsn["apks"]:
                 apk_url = None if apk["apk_url"] == NOAPK else url_with_replacements(apk["apk_url"], tag, tag_pattern)
                 prov = apk["provisioning"]
-                debug = [line for line in apk["build"]]
-                print(debug, file=sys.stderr)
                 versions.append(BuildRecipe(
                     repository=data["repository"],
                     tag=tag,
