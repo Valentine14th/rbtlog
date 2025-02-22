decd9cd55ee41e1e432c2f87acd822a68cc257b9
Laura Valentine Tscharner <laura.tscharner@tecnico.ulisboa.pt>
2025-02-22 17:57:07 +0100

    add builder username on log

--- scripts/build.py
+++ scripts/build.py
@@ -204,6 +204,7 @@ def build_with_backend(backend: BuildBackend, appid: str, recipe: BuildRecipe, *
     #     recipe = dataclasses.replace(recipe, apk_url=None if apk_url == NOAPK else apk_url)
     result: Dict[str, Any] = dict(
         appid=appid, version_code=None, version_name=None, tag=recipe.tag, commit=commit,
+        builder=os.getenv("GITHUB_ACTOR_ID"),
         recipe=recipe.for_json(), timestamp=int(time.time()), cpu_count=cpu_count,
         reproducible=None, error=None, build_log="", upstream_signed_apk_sha256=None,
         built_unsigned_apk_sha256=None, signature_copied_apk_sha256=None)
